62d25553172e63b6c7feedd0dcac03e05e5fa563
Ahmad Nidzam Musthafha <54913708+nidzammst@users.noreply.github.com>
2024-04-23 16:57:39 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
             src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5532061638278565'
           crossOrigin="anonymous"
         />
-        <meta name="google-site-verification" content="xtxQ4BivrvG0Zu1kxTrKzx7pABt7fxPSVix3uJKVjl8" />
+        <meta name="google-site-verification" content="JPjH3p9BMSCeUlaY_WKljnkhGyhfdEAuhCS51H4PP84" />
         </Head>
 
         <body className={`${BLOG.FONT_STYLE} tracking-wider bg-day dark:bg-night`}>
