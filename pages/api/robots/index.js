3bcc4edceaefe1c2faf6d92f928501191eef0457
Ahmad Nidzam Musthafha <54913708+nidzammst@users.noreply.github.com>
2024-04-23 02:25:13 +0000

    second update file pages/api/robots.js to pages/api/robots/index.js

--- pages/api/robots/index.js
+++ pages/api/robots/index.js
@@ -1,3 +1,5 @@
 export default function handler(req, res) {
-    res.send(`User-agent: *\nAllow: *\n# Host\nHost: https://mstblog.vercel.app/\n\nSitemap: https://mstblog.vercel.app/sitemap.xml`);
+  const textSiteMap =
+    'User-agent: *\nAllow: *\n# Host\nHost: https://mstblog.vercel.app\n\nSitemap: https://mstblog.vercel.app/sitemap.xml'
+  res.send(textSiteMap)
 }
